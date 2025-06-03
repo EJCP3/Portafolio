9302d23e70bee42ffd45418c8a8c83fee1a760d9
EJCP3 <euddycabrera03@gmail.com>
2025-06-03 11:43:55 -0400

    v4.9

--- src/components/data/Tags.ts
+++ src/components/data/Tags.ts
@@ -13,164 +13,85 @@ import Daisyui from "~/icons/DaisyUI.astro";
 import Tailwindcss from "~/icons/tailwindcss.astro";
 
 
-// export const TAGS = {
-//   react: {
-//     name: "React.js",
-//     class: "bg-[#61DAFB]/10 text-[#61DAFB] ring-[#61DAFB]/10",
-//     img: "/React_dark.svg",
-//     icon: React,
-//   },
-//   astro: {
-//     name: "Astro",
-//     class: "bg-[#FF5D01]/10 text-[#FF5D01] ring-[#FF5D01]/10",
-//     img: "/astro.svg",
-//     icon: Astro,
-//   },
-//   css: {
-//     name: "CSS",
-//     class: "bg-[#1572B6]/10 text-[#1572B6] ring-[#1572B6]/10",
-//     img: "/css.svg",
-//     icon: Css,
-//   },
-//   html: {
-//     name: "HTML5",
-//     class: "bg-[#E44D26]/10 text-[#E44D26] ring-[#E44D26]/10",
-//     img: "/html5.svg",
-//     icon: Html,
-//   },
-//   javascript: {
-//     name: "JavaScript",
-//     class: "bg-[#F7DF1E]/10 text-black ring-[#F7DF1E]/10", // texto negro por contraste
-//     img: "/javascript.svg",
-//     icon: JavaScript,
-//   },
-//   Sass: {
-//     name: "Sass",
-//     class: "bg-[#CD6799]/10 text-[#CD6799] ring-[#CD6799]/10",
-//     img: "/sass.svg",
-//     icon: Sass,
-//   },
-//   ReactSuite: {
-//     name: "React Suite",
-//     class: "bg-[#3498FF]/10 text-[#3498FF] ring-[#3498FF]/10",
-//     img: "/Suite.svg",
-//     icon: Suite,
-//   },
-//   figma: {
-//     name: "Figma",
-//     class: "bg-[#F24E1E]/10 text-[#F24E1E] ring-[#F24E1E]/10",
-//     img: "/figma.svg",
-//     icon: Figma,
-//   },
-//   github: {
-//     name: "GitHub",
-//     class: "bg-[#181717]/10 text-[#181717] ring-[#181717]/10",
-//     img: "/Github.svg",
-//     icon: GitHub,
-//   },
-//   supabase: {
-//     name: "Supabase",
-//     class: "bg-[#3ECF8E]/10 text-[#3ECF8E] ring-[#3ECF8E]/10",
-//     img: "/supabase.svg",
-//     icon: Supabase,
-//   },
-//   daisyui: {
-//     name: "DaisyUI",
-//     class: "bg-[#A855F7]/10 text-[#A855F7] ring-[#A855F7]/10",
-//     img: "/daisyui.svg",
-//     icon: Daisyui,
-//   },
-//   vite: {
-//     name: "Vite",
-//     class: "bg-[#646CFF]/10 text-[#646CFF] ring-[#646CFF]/10",
-//     img: "/vitejs.svg",
-//     icon: Vitejs,
-//   },
-//   tailwindcss: {
-//     name: "Tailwind CSS",
-//     class: "bg-[#06B6D4]/10 text-[#06B6D4] ring-[#06B6D4]/10",
-//     img: "/tailwindcss.svg",
-//     icon: Tailwindcss,
-//   },
-// };
+
 
 export const TAGS = {
   react: {
     name: "React.js",
     class: "badge-info bg-info",
-    img: "/React_dark.svg",
+    img: "/sprite.svg#react",
     icon: React,
   },
   astro: {
     name: "Astro",
     class: "badge-error",
-    img: "/astro.svg",
+    img: "/sprite.svg#astro",
     icon: Astro,
   },
   css: {
     name: "CSS",
     class: "badge-info",
-    img: "/css.svg",
+    img: "/sprite.svg#css",
     icon: Css,
   },
   html: {
     name: "HTML5",
     class: "badge-error",
-    img: "/html5.svg",
+    img: "/sprite.svg#html",
     icon: Html,
   },
   javascript: {
     name: "JavaScript",
     class: "badge-warning", // texto negro por contraste
-    img: "/javascript.svg",
+    img: "/sprite.svg#javacript",
     icon: JavaScript,
   },
   Sass: {
     name: "Sass",
     class: "badge-secondary",
-    img: "/sass.svg",
+    img: "/sprite.svg#sass",
     icon: Sass,
   },
   ReactSuite: {
     name: "React Suite",
     class: "badge-info",
-    img: "/Suite.svg",
+    img: "/sprite.svg#suite",
     icon: Suite,
   },
   figma: {
     name: "Figma",
     class: "badge-error",
-    img: "/figma.svg",
+    img: "/sprite.svg#figma",
     icon: Figma,
   },
   github: {
     name: "GitHub",
     class: "badge-neutral",
-    img: "/Github.svg",
+    img: "/sprite.svg#github",
     icon: GitHub,
   },
   supabase: {
     name: "Supabase",
     class: "badge-success",
-    img: "/supabase.svg",
+    img: "/sprite.svg#supabase",
     icon: Supabase,
   },
   daisyui: {
     name: "DaisyUI",
     class: "badge-primary",
-    img: "/daisyui.svg",
+    img: "/sprite.svg#daisyui",
     icon: Daisyui,
   },
   vite: {
     name: "Vite",
     class: "badge-primary",
-    img: "/vitejs.svg",
+    img: "/sprite.svg#vite",
     icon: Vitejs,
   },
   tailwindcss: {
     name: "Tailwind CSS",
     class: "badge-info",
-    img: "/tailwindcss.svg",
+    img: "/sprite.svg#tailwind",
     icon: Tailwindcss,
   },
 };
